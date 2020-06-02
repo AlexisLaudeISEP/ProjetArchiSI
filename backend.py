52c75c76bc8b5537ce9256925d7fa32bbd0c7fd5
vincent tellier <you@example.comvincent.tellier@isep.fr git config --global user.email you@example.comvincent.tellier@free.fr>
2020-06-02 11:43:35 +0200

    nbr_point
    Signed-off-by: vincent tellier <you@example.comvincent.tellier@isep.fr git config --global user.email you@example.comvincent.tellier@free.fr>

--- backend.py
+++ backend.py
@@ -134,6 +134,7 @@ def nbr_point(idEquipe):
     cursor.execute("SELECT * FROM Schema1.Match WHERE (idEquipe1="+chaine+" OR idEquipe2="+chaine+") AND ButEquipe1 != -1 ORDER BY Date DESC ")
     for row in cursor:
         res.append(result(idEquipe,row))
+    print (res)
     for i in range(len(res)):
         if res[i] == 'V':
             count+=3
